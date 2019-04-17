7b8c8e2d2e9d569ed7d5fab353d5efd2a9ef3d6a
velurubharath <velurubharath@gmail.com>
2019-04-17 22:59:06 +0530

    1.3

--- DataStructures/src/coding/ArrayAndStrings/URLify.java
+++ DataStructures/src/coding/ArrayAndStrings/URLify.java
@@ -5,24 +5,48 @@ public class URLify {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String sentence = "Bharath is a good boy";
-		String result = replaceTheWhiteSpaces(sentence);
-		System.out.println("Result is "+ result);
+		String sentence = "Bharath is a good boy   ";
+		char[] sentenceArray = sentence.toCharArray();
+		
+		int spaces = findBlankSpaces(sentenceArray);
+		System.out.println("Blank spaces in the sentence "+spaces);
+		
+		int truelength = 50;
+		
+		replaceSpaces(char[] sentenceArray,int truelength, int spaces);
 		
 	}
 
-	private static String replaceTheWhiteSpaces(String sentence) {
+	private static void replaceSpaces(char[] sentenceArray, int truelength, int spaces) {
 		// TODO Auto-generated method stub
 		
-		char[] array = sentence.toCharArray();
-		for(int i=0;i<sentence.length();i++){
-			if(array[i]==' ')
-				array[i]='c';
-			else{
-				System.out.println("No white spaces are found");
+		int index= trulength+spaces*2;
+		if(truelength<str.lenth) str[truelength]='\0';
+		for(int i=trulength-1;i>=0;i--){
+			if(sentenceArray[i]==' '){
+				sentenceArray[index-1]='0';
+				sentenceArray[index-2]='2';
+				sentenceArray[index-3]='%';
+				index-=3;
+			}else{
+				sentenceArray[index-1]=sentenceArray[i];
+				index--;
 			}
 		}
-		return array.toString();
+		
 	}
+
+	private static int findBlankSpaces(char[] sentenceArray) {
+		// TODO Auto-generated method stub
+		int count =0;
+		
+		for(int i=0;i<sentenceArray.length;i++){
+			if(sentenceArray[i]==' ')
+				count++;
+		}
+		
+		return count;
+	}
+
 
 }
